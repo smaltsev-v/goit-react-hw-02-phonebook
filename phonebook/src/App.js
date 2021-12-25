5d42c560ac17b3ecd42f4dc393dd0eaca17087d9
Serhii Maltsev <83024347+smaltsev-v@users.noreply.github.com>
2021-12-25 23:46:42 +0200

    add components
    добавил компонент

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,4 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import ContactForm from "./componets/ContactF";
 
+
+import { v4 as uid } from "uuid";
+
+
+class App extends Component {
+  state = {
+    contacts: contacts,
+    filter: "",
+  };
+
+  formSubmitHandler = ({ name, number }) => {
+    const { contacts } = this.state;
+
+    if (contacts.some((contact) => contact.name === name)) {
+      alert(
+        `${name} is already in contacts. Want to replace an existing contact ?`
+      );
+      return;
+    }
+    const id = uid();
+    this.setState({
+      contacts: [{ name, number, id }, ...contacts],
+      filter: "",
+    });
+
+    console.log({ name, number });
+  };
+
+  
+
+  
+
+  render() {
+    
+
+    return (
+      <div className="container">
+        <h1 className="title">Phonebook</h1>
+        <ContactForm onSubmit={this.formSubmitHandler} />
+      </div>
+    );
+  }
+}
 
 export default App;
